4e8056d3884b467475aa0687cb113e437d92386b
SantiagoBetancurCardona97 <santibetancur97@gmail.com>
2021-02-21 19:28:22 -0500

    Clean proyect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import {isEmpty} from 'lodash'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [task, setTask] = useState("")
+
+ const addTask = (e) => {
+  e.preventDefault()
+  if (isEmpty(task)) {
+    console.log("Task empty")
+    return
+  }
+  console.log("Ok")
+  setTask("")
+ }
+ return (
+ <div className="container mt-5">
+  <h1>Tareas</h1>
+  <hr/>
+  <div className="row">
+    <div className="col-8">
+      <h4 className="text-center">Lista de Tareas</h4>
+      <ul className="list-gruop">
+        <li className="list-gruop-item">
+          <span className="lead">Nombre de Tarea</span>
+          <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+          <button className="btn btn-warning btn-sm float-right">Editar</button>
+        </li>
+      </ul>
     </div>
-  );
+    <div className="col-4">
+      <h4 className="text-center">Formulario</h4>
+      <form onSubmit={addTask}>
+        <input
+        type="text"
+        className="form-control mb-2"
+        placeholder="Ingrese Tarea ..."
+        onChange={(text) => setTask(text.target.value)}
+        value={task}
+        />
+       <button 
+        className="btn btn-dark btn-block"
+        type="submit">
+        Agregar
+       </button> 
+      </form>
+    </div>
+  </div>
+ </div>
+  )
 }
 
 export default App;

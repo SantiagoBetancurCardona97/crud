488ea878fb40617c8ffdddd40dde60850d889941
SantiagoBetancurCardona97 <santibetancur97@gmail.com>
2021-02-25 22:28:52 -0500

    Add task
    Video 15

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React,{useState} from 'react'
 import {isEmpty} from 'lodash'
+import shortid from 'shortid'
 
 
 function App() {
  const [task, setTask] = useState("")
+ const [tasks, setTasks] = useState([])
 
- const addTask = (e) => {
+  const addTask = (e) => {
   e.preventDefault()
   if (isEmpty(task)) {
     console.log("Task empty")
     return
   }
-  console.log("Ok")
+  
+  const newTask = {
+    id: shortid.generate(),
+    name: task
+  }
+
+  setTasks([...tasks, newTask ])
   setTask("")
  }
+
  return (
  <div className="container mt-5">
   <h1>Tareas</h1>
@@ -22,11 +31,15 @@ function App() {
     <div className="col-8">
       <h4 className="text-center">Lista de Tareas</h4>
       <ul className="list-gruop">
-        <li className="list-gruop-item">
-          <span className="lead">Nombre de Tarea</span>
-          <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
-          <button className="btn btn-warning btn-sm float-right">Editar</button>
-        </li>
+       { 
+        tasks.map((task) => (
+         <li className="list-group-item">
+           <span className="lead">{task.name}</span>
+           <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+           <button className="btn btn-warning btn-sm float-right">Editar</button>
+         </li>
+        ))
+       }
       </ul>
     </div>
     <div className="col-4">
